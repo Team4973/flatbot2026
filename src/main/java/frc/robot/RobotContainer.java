dd5c09a11fcade54b10ee3a49c87adfd2cfdd2ca
Cosimo Aho <cosimoaho@gmail.com>
2026-01-08 10:43:14 -0800

    add code for changing speeds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,25 +8,42 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 public class RobotContainer {
 
-  public final TalonFX m1 = new TalonFX(30);
+  public final TalonFX m1 = new TalonFX(30); // change device ID to match your configuration
   public final CommandXboxController input_interface = new CommandXboxController(0);
+
+  public double speed = 0.1; // initalize the speed as 0.1
 
     public RobotContainer() {
     runMotor();
+    changeSpeed();
     }
 
     private void runMotor() {
       input_interface.a().onTrue(
         new InstantCommand(() -> {
-          System.out.println("m1 set at 0.5");
-          m1.set(0.5);
+          System.out.println("m1 set at" + speed);
+          m1.set(speed); // spin up motor 1 at 0.1 speed
         })
       );
       input_interface.a().onFalse(
         new InstantCommand(() -> {
-          m1.set(0.0);
+          m1.set(0.0); // set motor off when a is not true
         })
       );
+    }
+
+    private void changeSpeed() {
+      input_interface.povUp().onTrue(
+        new InstantCommand(() -> {
+          speed += 0.05;
+        })
+      );
+      input_interface.povDown().onTrue(
+        new InstantCommand(() -> {
+          speed -= 0.05;
+        })
+      );
+      
     }
 }
 
